894b2f8d4998ac52866caf9705dd528a433c8937
ranaabhishek1 <abhishekforbusiness1@gmail.com>
2024-03-21 23:38:37 +0530

    change structure

--- noida.js
+++ noida.js
@@ -27,17 +27,17 @@ fornav();
 
 // 
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  
+});
 
 // 
