da09b5ba613d79d9876910e5b0ca37e8945ceeee
ranaabhishek1 <abhishekforbusiness1@gmail.com>
2024-04-04 23:24:51 +0530

    work continue

--- noida ref.js
+++ noida ref.js
@@ -41,3 +41,34 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 // 
+
+
+// for brand hover 
+
+var brand=document.querySelector(".nav-brand a");
+var brandHover=document.querySelector(".brand-hover");
+
+
+brand.addEventListener("mouseenter",function(){
+  brandHover.style.opacity="1";
+
+
+  brandHover.addEventListener("mouseenter",function(){
+    brandHover.style.opacity="1";
+  
+  })
+})
+
+
+
+
+
+
+brand.addEventListener("mouseleave",function(){
+  brandHover.style.opacity="0";
+})
+
+
+
+
+// end brand hover 
