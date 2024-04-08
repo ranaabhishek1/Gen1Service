34e7ec7ec307cdde82687b9f7c2dd8dab5d82e53
ranaabhishek1 <abhishekforbusiness1@gmail.com>
2024-04-08 20:05:52 +0530

    some changes in noida refergetor page

--- noida ref.js
+++ noida ref.js
@@ -43,7 +43,7 @@ var swiper = new Swiper(".mySwiper", {
 // 
 
 
-// for brand hover 
+// for brand hover 1
 
 var brand=document.querySelector(".nav-brand a");
 var brandHover=document.querySelector(".brand-hover");
@@ -59,16 +59,61 @@ brand.addEventListener("mouseenter",function(){
   })
 })
 
-
-
-
-
-
 brand.addEventListener("mouseleave",function(){
   brandHover.style.opacity="0";
 })
 
 
+// end brand hover 1
 
 
-// end brand hover 
+// start Brand Hover 2
+
+var brand2=document.querySelector(".brand-btn a");
+var brandHover2=document.querySelector(".select-brand-hover");
+
+
+brand2.addEventListener("mouseenter",function(){
+  brandHover2.style.opacity="1";
+
+
+  brandHover2.addEventListener("mouseenter",function(){
+    brandHover2.style.opacity="1";
+  
+  })
+})
+
+brand2.addEventListener("mouseleave",function(){
+  brandHover2.style.opacity="0";
+})
+
+
+
+
+// end brand hover 2
+
+
+// start Brand Hover 3
+
+var brand3=document.querySelector(".other-brand a");
+var brandHover3=document.querySelector(".select-brand-hover");
+
+
+brand3.addEventListener("mouseenter",function(){
+  brandHover3.style.opacity="1";
+
+
+  brandHover3.addEventListener("mouseenter",function(){
+    brandHover3.style.opacity="1";
+  
+  })
+})
+
+brand3.addEventListener("mouseleave",function(){
+  brandHover3.style.opacity="0";
+})
+
+
+
+
+// end brand hover 3
